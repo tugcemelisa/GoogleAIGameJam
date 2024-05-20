30b6cd220a36054f189fa287adf47f0e69075167
Melisa Tugce <tugcehaspolat@hotmail.com>
2024-05-20 14:16:37 +0300

    Add: Terrain, Scene, Material

---
+++ Project/Assets/_Game/Scripts/ComprobacionTag.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ComprobacionTag : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(tag);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Project/Assets/_Game/Scripts/EscalaTiempo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscalaTiempo : MonoBehaviour
+{
+    bool pausa; // si es false No hay pausa, si es true, estará pausado
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F10))
+        {
+            if (pausa == false)
+            {
+                Time.timeScale = 0;
+                pausa = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pausa = false;
+           
+            }
+
+
+
+        }
+           
+        
+    }
+}
